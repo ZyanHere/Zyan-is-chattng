7a9c562fd22a0b68790724b843ff80c21259ee43
Zyan <craftzyan@gmail.com>
2026-08-02 11:57:44 +0530

    chat service added

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import proxy from "express-http-proxy"
+import { proxyWithHeader } from "./utils/proxyWithHeader.js"
 dotenv.config()
 
 const port = process.env.PORT
@@ -13,6 +15,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use("/auth", proxy(process.env.AUTH_SERVICE))
+app.use("/chat", proxyWithHeader(process.env.CHAT_SERVICE))
 app.get("/api/me",protect,getCurrentUser)
 app.get("/",(req,res)=>{
     res.json({message:"hello from gateway v5"})
